7740a4c9f12ad8e349ae1ebd714ae0d6307c2e6d
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-07-23 13:53:42 -0400

    #03 - add demo.R

--- cassandra_tutorials/scripts/01_demo.R
+++ cassandra_tutorials/scripts/01_demo.R
@@ -25,19 +25,50 @@ options(future.fork.enable = TRUE)
 # Create Files ----
 create_files <- TRUE
 
+# Robyn Directory ----
+robyn_directory <- "~/Desktop/School/2023_projects_"
+
 # *****************************************************************************
 # **** ----
 # DATA IMPORT ----
 # *****************************************************************************
 
+# * Weekly Data ----
+data("dt_simulated_weekly")
 
+dt_simulated_weekly <- dt_simulated_weekly %>% 
+    clean_names() %>% 
+    as_tibble()
+
+# * Holiday Data ----
+data("dt_prophet_holidays")
+
+dt_prophet_holidays <- dt_prophet_holidays %>% 
+    clean_names() %>% 
+    as_tibble()
+
+dt_prophet_holidays %>% distinct(country) %>% print(n = 100)
+
+# * Notes ----
+# - date = date variable
+# - revenue = revenue variable
+# - variables ending with "_s" = spend variables
+# - variables ending with "_i" = impressions. Impressions recommended over clicks
+# - competitor_sales_be = competitor variables
+# - events = events variables
+# - newsletter = newsletter variable
 
 
 
 # *****************************************************************************
 # **** ----
-# SECTION NAME ----
+# MODEL SPECIFICATION ----
 # *****************************************************************************
+
+
+
+
+
 # *****************************************************************************
 # **** ----
 # SECTION NAME ----
